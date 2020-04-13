698e2f15ea3794e3a5734a345f746152881d1bef
Danilo Menezes <danilo.menezes@sky.uk>
2020-04-13 15:23:06 +0100

    remove unused methods vars and deps

--- scrapper/index.js
+++ scrapper/index.js
@@ -1,70 +1,7 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
-const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
 const allItemsJson = require('./files/items.json');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const { camelCase, zipObject, groupBy, map, keyBy, mapValues } = require('lodash');
-const { googleApiKey, sourceSheetId } = require('../config/vars');
-
-
-const doc = new GoogleSpreadsheet(sourceSheetId);
-
-const getUrl = (sheet, range) => `https://sheets.googleapis.com/v4/spreadsheets/${sourceSheetId}/values/${sheet}!${range}?key=${googleApiKey}&valueRenderOption=FORMULA`;
-
-const scrapeVariants = html => {
-    const $ = cheerio.load(html);
-    const imgDivs = $('div[class="post-image-container"]')
-    return imgDivs.map((i, div) => $(div).attr().id)
-}
-
-const fetchVariants = async (collections) => {
-    for (collection of collections) {
-        const variantItems = collection.values.filter(item => item.hasVariant);
-
-        for (variantItem of variantItems) {
-            const content = await request(variantItem.variant);
-            const variants = scrapeVariants(content);
-
-            variantItem.variant = variants;
-        }
-    }
-}
-
-const parseRawData = rawData => {
-    const parsed = rawData.map(({ type, values }) => {
-        return {
-            type,
-            values: values.map(item => {
-                const { image, variant } = item
-
-                if (image && image.includes("=image(\"")) {
-                    item.image = image.replace("=image(\"", "").replace("\")", "");
-                }
-
-                if (variant && variant.includes("=HYPERLINK(\"")) {
-                    let itemVariant = variant.replace("=HYPERLINK(", "").replace("\"", "").replace(")", "").split("\"").join("").split(",");
-                    if (itemVariant[1] === "Yes") {
-                        item.variant = itemVariant[0]
-                        item.hasVariant = true
-                    } else {
-                        item.hasVariant = false
-                        delete item.variant
-                    }
-                } else {
-                    item.hasVariant = false
-                }
-
-                return {
-                    ...item
-                }
-            })
-        }
-    })
-
-    fetchVariants(parsed)
-}
+const { camelCase, zipObject, groupBy, keyBy, mapValues } = require('lodash');
 
 const transformCsv = location => {
     const file = fs.readFileSync(path.join(__dirname, location), 'utf8').split('\n');
@@ -109,52 +46,6 @@ const fillItemsData = (items, fullItemDetails) => {
             }
         }
     });
-}
-
-const parseRecipes = (recipesRaw, fullItemDetails) => {
-    const materialRef = zipObject(map(fullItemDetails, 'primaryKey'), map(fullItemDetails, 'ingameName'));
-
-    return recipesRaw.map(recipe => {
-        const qtyKeys = Object.keys(recipe).slice(8, 14).map(key => Number(recipe[key]));
-        const idKeys = Object.keys(recipe).slice(14).map(key => recipe[key]);
-        const materials = qtyKeys.filter(qty => qty > 0).map((qty, i) => {
-            const materialId = idKeys[i]
-            return { qty: qty, materialId, materialName: materialRef[materialId] }
-        });
-
-        return {
-            itemId: recipe.itemId,
-            materials
-        }
-    })
-}
-
-exports.scrapeGoogleDocs = async () => {
-    await doc.useApiKey(googleApiKey);
-    await doc.loadInfo();
-
-    const sheets = doc.sheetsByIndex.slice(1);
-    const mapped = [];
-
-    for (sheet of sheets) {
-        const { title, lastColumnLetter, rowCount } = sheet;
-        const range = `A1:${lastColumnLetter}${rowCount}`;
-
-        const { data } = await axios.get(getUrl(title, range));
-        if (!data || data.values.length == 0) {
-            return;
-        }
-
-        const keys = data.values.shift().map(key => camelCase(key));
-        const values = data.values;
-
-        mapped.push({
-            type: camelCase(title),
-            values: values.map(row => zipObject(keys, row))
-        });
-    }
-
-    parseRawData(mapped);
 }
 
 exports.scrapeLocalFiles = async () => {
