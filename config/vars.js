a79b6336e1ff6be9f48eee974859233469a1fa86
Danilo Menezes <danilo.menezes@sky.uk>
2020-04-20 02:14:32 +0100

    remove unused vars

--- config/vars.js
+++ config/vars.js
@@ -1,9 +1,6 @@
 require('dotenv-safe').config();
 
 module.exports = {
-    mongo: {
-        uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
-    },
     googleApiKey: process.env.GOOGLE_API_KEY,
     sourceSheetId: process.env.SOURCE_SHEET_ID,
     port: process.env.PORT
